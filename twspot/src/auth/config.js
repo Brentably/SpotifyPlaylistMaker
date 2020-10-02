7517819e24303a57bfff022deffc6467b6b46692
Brentably <Brent@pabnet.com>
2020-10-02 16:43:48 -0400

    added implicit grant flow api functionality

---
+++ twspot/src/auth/config.js
@@ -0,0 +1,27 @@
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "4451d90a4f634c33b6f4e015d616ab44";
+export const redirectUri = "http://localhost:3000/home";
+export const scopes = [
+    "ugc-image-upload",
+    "user-top-read",
+    "user-read-playback-position",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-read-currently-playing",
+    "user-modify-playback-state",
+    "user-library-modify",
+    "user-library-read",
+    "streaming",
+    "app-remote-control",
+    "user-read-private",
+    "user-read-email",
+    "user-follow-modify",
+    "user-follow-read",
+    "playlist-modify-public",
+    "playlist-read-collaborative",
+    "playlist-read-private",
+    "playlist-modify-private"
+];
--- twspot/src/pages/Home.js
+++ twspot/src/pages/Home.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import hash from "../auth/hash"
+import { useHistory } from 'react-router-dom'
+
 
 
 const Home = () => {
+  let history = useHistory()
+
+  const [token, setToken] = useState(null)
+
+  const spotifyConnect = async () => {
+    if(!token) return
+    let response = await fetch('https://api.spotify.com/v1/me', {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }})
+    console.log(response)
+    console.log(token)
+  }
+
+
+  useEffect(() => {
+    let _token = hash.access_token;
+    // redirects back to the login page if there is an error and no token for now
+    if (!_token) history.push("/")
+
+    setToken(_token)
+    // console.log(_token)
+    spotifyConnect()
+  }, [history, token, spotifyConnect])
+
     return (
       <div className="bg-black p-5 rounded-lg text-center max-w-xs">
         <h1 className="text-white text-2xl mb-2">Welcome to the Home page</h1>
-        {/* <h5 className="text-gray-200 text-wrap mb-4">This app allows you to create playlists based on the BPM of songs to create perfectly customized running playlists.</h5> */}
       
       </div>
     );
