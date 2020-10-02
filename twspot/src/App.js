aac9c8ec7fd985a22d853c95f93ff07b559ba86f
Brentably <Brent@pabnet.com>
2020-10-02 14:41:04 -0400

    added React router, and a faux home page to redirect to after spotify authentication. next step is setting up the authentication

--- twspot/src/App.js
+++ twspot/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Login from './pages/Login'
+import Home from './pages/Home'
+import {Switch, Route} from "react-router-dom"
 
-const auth = () => {
-  console.log("wazzuuup")
-}
 
 
 function App() {
   return (
-    <div className="bg-black p-5 rounded-lg text-center max-w-xs">
-      <h1 className="text-white text-2xl mb-2">BPM Playlist Maker</h1>
-      <h5 className="text-gray-200 text-wrap mb-4">This app allows you to create playlists based on the BPM of songs to create perfectly customized running playlists.</h5>
-    <button className="btn text-white bg-green-700 hover:bg-green-800" onClick={auth}>
-      Connect to Spotify
-    </button>
+    <div>
+      <Switch>
+          <Route exact path="/">
+              <Login />
+          </Route>
+          <Route path="/home">
+              <Home/>
+          </Route>
+      </Switch>
     </div>
   );
 }
---
+++ twspot/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+      <div className="bg-black p-5 rounded-lg text-center max-w-xs">
+        <h1 className="text-white text-2xl mb-2">Welcome to the Home page</h1>
+        {/* <h5 className="text-gray-200 text-wrap mb-4">This app allows you to create playlists based on the BPM of songs to create perfectly customized running playlists.</h5> */}
+      
+      </div>
+    );
+  }
+
+
+
+export default Home
