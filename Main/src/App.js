90d7bf8f056c260c18b80a9c0c90dd4d09b0f8cb
Brentably <Brent@pabnet.com>
2020-10-05 17:02:12 -0400

    made some final changes to revert it back to where it was before I messed it up

--- Main/src/App.js
+++ Main/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import Login from './pages/Login'
-// import Home from './pages/Home'
-// import {Switch, Route} from "react-router-dom"
+import Home from './pages/Home'
+import {Switch, Route} from "react-router-dom"
 
 
 
 function App() {
   return (
     <div>
-      <Login />
-      {/* <Switch>
+      <Switch>
           <Route exact path="/">
               <Login />
           </Route>
           <Route path="/home">
               <Home/>
           </Route>
-      </Switch> */}
+      </Switch>
     </div>
   );
 }
