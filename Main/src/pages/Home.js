79a43799f7aaaa151a2e2d1172f0e01278bffaea
Brentably <Brent@pabnet.com>
2020-10-05 17:23:27 -0400

    improved error handling and flow for token authentication

--- Main/src/pages/Home.js
+++ Main/src/pages/Home.js
@@ -1,42 +1,38 @@
 import React, { useState, useEffect} from 'react';
 import hash from "../auth/hash"
-import { useHistory } from 'react-router-dom'
 
 
 
 const Home = () => {
-  // this is a hook given by react router dom-- I use it later to send the user back to the login page if the login didn't work
-  let history = useHistory()
+  hash.error && console.log(hash.error)
 
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(hash.access_token)
   const [userData, setUserData] = useState(null)
-  const spotifyConnect = async () => {
-    if(!token) return
-    let response = await fetch('https://api.spotify.com/v1/me', {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }})
-    setUserData(response.json())
-  }
+
+  
 
 
   useEffect(() => {
-    let _token = hash.access_token;
-    console.log(_token)
-    // redirects back to the login page if there is no token
-    if (!_token) history.push("/")
-
-    setToken(_token)
+      const spotifyConnect = async () => {
+        if(!token) return
+        let response = await fetch('https://api.spotify.com/v1/me', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }})
+        setUserData(response.json())
+      }
     // console.log(_token)
+    // redirects back to the login page if there is no token
+
+
     spotifyConnect()
-  }, [history, token, spotifyConnect])
+  }, [history, token])
 
     return (
       <div className="bg-black p-5 rounded-lg text-center max-w-xs">
         <h1 className="text-white text-2xl mb-2">Welcome to the Home page</h1>
-      
       </div>
     );
   }
