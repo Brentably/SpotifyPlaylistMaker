eb84ae62bcb3e18c11c5ed1f741f0309bd87c551
Brentably <Brent@pabnet.com>
2020-10-09 13:50:53 -0400

    no header on small devices like phones

--- main-bootstrap/src/components/Header.js
+++ main-bootstrap/src/components/Header.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Header = () => {
     return (
         <>
-        <div className="text-white text-center fredoka-font p-2">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl">
+        <div className="text-white text-center fredoka-font p-2 h-0 sm:h-auto">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl hidden sm:inlinegi">
                 <img src={require("../Icons/runnerman.svg")} alt="music notes" className="inline mr-2" style={{verticalAlign: 'baseline', height: "0.9em"}}/>
                 BPM Playlist Maker
             </h1>
