bf1a803e8ceea120e0940225dca9ba2248977f83
Brentably <Brent@pabnet.com>
2020-10-09 15:42:34 -0400

    Updates

---
+++ precache-manifest.276f94045ac14296e3cd57925bf635c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdd3ed701c350ecada4b1e7548a52b8",
+    "url": "/SpotifyPlaylistMaker/index.html"
+  },
+  {
+    "revision": "7a38e7a8c28989f5c279",
+    "url": "/SpotifyPlaylistMaker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "84019fb1fcc9f1478a2a",
+    "url": "/SpotifyPlaylistMaker/static/css/main.7a1cf9a9.chunk.css"
+  },
+  {
+    "revision": "7a38e7a8c28989f5c279",
+    "url": "/SpotifyPlaylistMaker/static/js/2.2b4f9fb5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SpotifyPlaylistMaker/static/js/2.2b4f9fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84019fb1fcc9f1478a2a",
+    "url": "/SpotifyPlaylistMaker/static/js/main.92dafc5a.chunk.js"
+  },
+  {
+    "revision": "2a2f728cdd39f1c14597",
+    "url": "/SpotifyPlaylistMaker/static/js/runtime-main.2dffec97.js"
+  },
+  {
+    "revision": "4a2f2ea45a0bb1abe81b47d0afde4aae",
+    "url": "/SpotifyPlaylistMaker/static/media/FredokaOne-Regular.4a2f2ea4.ttf"
+  },
+  {
+    "revision": "6fd522490ec7674ec019b3b68ac036a9",
+    "url": "/SpotifyPlaylistMaker/static/media/runnerman.6fd52249.svg"
+  }
+]);
