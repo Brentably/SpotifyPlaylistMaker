00e58340683070f2933ddbf15cc4ca6b1fcff82a
Brentably <Brent@pabnet.com>
2020-10-11 16:30:01 -0400

    deleted some comments, trying to migrate home.js into app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-// import Login from './components/Login'
 import Home from './Home'
-// import {Switch, Route} from "react-router-dom"
-// import hash from './auth/hash';
-// not using react router rn
 
 
 function App() {
-  // if(!hash.access_token) return <Login />
   return (
     <div>
-      {/* <Switch> */}
-          {/* <Route exact path="/"> */}
               <Home />
-          {/* </Route> */}
-      {/* </Switch> */}
     </div>
   );
 }
--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -4,7 +4,7 @@ const PlaylistCard = (props) => {
     // console.log(props)
     return (
         <div className="text-white flex m-2 font-nunito-semibold ellipsis items-center">
-            <img src={props.images[0].url} alt="Playlist" className="mr-2" style={{objectFit: "cover", maxHeight: "3.5em"}} />
+            <img src={props.images[0].url} alt="Playlist" className="mr-2" style={{objectFit: "cover", height: "3.5em", width: "3.5em"}} />
             <div className="ellipsis">
             <div className="text-md ellipsis">{props.name}</div>
             <div className="text-sm text-silver">by {props.owner.display_name}</div>
