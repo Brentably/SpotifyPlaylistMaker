b61c6d2cb3308bd1ae364c7793b48235c407d666
Brentably <Brent@pabnet.com>
2020-10-14 10:09:01 -0400

    added hover classes to playlist cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ import defaultIcon from "../icons/defaultIcon.svg"
 const Card = (props) => {
     // console.log(props)
     return (
-        <div className="text-white flex m-2 font-nunito-semibold ellipsis items-center">
-            <img src={props.images[0] ? props.images[0].url : defaultIcon} alt="{props.musicType}" className="mr-2" style={{objectFit: "cover", height: "3.5em", width: "3.5em"}} />
+        <div id="card" className="text-white flex font-nunito-semibold ellipsis items-center" style={{margin: "0.6rem"}}>
+            <img src={props.images[0] ? props.images[0].url : defaultIcon} alt="{props.musicType}" className="mr-2" style={{objectFit: "cover", height: "3.4em", width: "3.4em"}} />
             <div className="ellipsis">
             <div className="text-md ellipsis">{props.name}</div>
             <div className="text-sm text-silver">by {props.owner.display_name}</div>
